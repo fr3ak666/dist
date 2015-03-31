00c551ba831d49266041ea1ebba1e71c56a518b8
fr3ak666 <piotr.kosecki@gmail.com>
2015-03-31 23:50:24 +0200

    something is nondeterministic

--- src/main/scala/dist.scala
+++ src/main/scala/dist.scala
@@ -1,14 +1,10 @@
 /**
  * Created by fr3ak on 3/31/15.
  */
-import scala.io._
 
 
 object dist extends App {
 
-  val in = Source.fromFile("/usr/share/dict/words")
-  val out = "dupa.txt"
-  val words = in.getLines.toList.par
-  Console.println(words.filter(x => x.contains("dup")))
-
+  val fst = (1 until 10000000 par) map (x => x*2) map (x => x / 3) map (x => x % 100000) reduce((a, b) => if(b%3==0) a+b else a-b)
+  println(fst)
 }
